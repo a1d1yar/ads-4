e1537ee4e2d6a856e1155643548ee898fcf4802f
Aldiyar <aldiyarmaratov0@gmail.com>
2023-05-15 00:58:51 +0600

    created Method put

--- MyHashTable.java
+++ MyHashTable.java
@@ -31,6 +31,20 @@ public class MyHashTable<K, V> {
         int result=(key.hashCode() & 0x7fffffff) % M;
         return result;
     }
-
+    public void put(K key,V value){
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = chainArray[index];
+        chainArray[index] = newNode;
+        size++;
+    }
 
 }
